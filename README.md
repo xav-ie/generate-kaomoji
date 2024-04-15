9f7a14806871e6e99fc66a5fb41c71cd67b2b325
xav-ie <github@xav.ie>
2024-04-15 09:12:49 -0400

    feat(kaomoji-generator): generate a random kaomoji

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
-# kaomoji
+# kaomoji-generator
+
+Generate random kaomoji! Useful for notifications, bars, and all other utilities for extra ╰( ͡° ͜ʖ ͡° )つ──☆*:・ﾟ .
+
+## Usage
+
+See `generate-kaomoji --help` or `nix run github:xav-ie/kaomoji-generator -- --help`:
+```sh
+generate-kaomoji --help
+Usage: generate-kaomoji [options] [JQ filter]
+
+Options:
+  --help            Show this help message and exit
+  -r, --raw-output  Output raw strings, not JSON texts
+  [JQ filter]       Apply a custom jq filter to the selected kaomoji
+
+Examples:
+
+# generate a kaomoji object
+> generate-kaomoji
+{
+  "category": "love",
+  "value": "(*˘︶˘*).｡.:*♡"
+}
+
+# select the raw kaomoji string
+> generate-kaomoji -r .value
+(*˘︶˘*).｡.:*♡
+```
+
+## About
+
 Kaomoji dataset scraped from kaomoji.ru.
 
 The fork opts to use json instead as primary format. There was some duplicates removed from the original.
@@ -6,3 +37,4 @@ The fork opts to use json instead as primary format. There was some duplicates r
 It is 757 kaomojis total at time of writing.
 
 I think the selection on kaomoji.ru is good. There are some places that have excessive/useless kaomoji. This list may change over time to have less/more kaomoji that are more relevant.
+
---
+++ flake.nix
@@ -0,0 +1,67 @@
+{
+  description = "Generate a random kaomoji";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    # Create system-specific outputs for the standard Nix systems
+    # https://github.com/numtide/flake-utils/blob/main/default.nix#L3-L9
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        myprogram = pkgs.writeShellApplication {
+          name = "generate-kaomoji";
+          runtimeInputs = [ pkgs.jq ];
+          text = ''
+            if [[ " $* " == *" --help "* ]]; then
+              echo "Usage: $(basename "$0") [options] [JQ filter]"
+              echo ""
+              echo "Options:"
+              echo "  --help            Show this help message and exit"
+              echo "  -r, --raw-output  Output raw strings, not JSON texts"
+              echo "  [JQ filter]       Apply a custom jq filter to the selected kaomoji"
+              echo ""
+              echo "Examples:"
+              echo ""
+              echo "# generate a kaomoji object"
+              example1=$("$0")
+              echo "> $(basename "$0")"
+              echo "$example1"
+              echo ""
+              echo "# select the raw kaomoji string"
+              example2=$(echo "$example1" | jq -r ".value") 
+              echo "> $(basename "$0") -r .value"
+              echo "$example2"
+              exit 0
+            fi
+
+            ARRAY=$(cat kaomoji.json)
+            LENGTH=$(echo "$ARRAY" | jq '.kaomoji | length')
+            RAND_INDEX=$((RANDOM % LENGTH))
+
+            RAW_OUTPUT=""
+            if [[ " $* " == *" -r "* ]] || [[ " $* " == *" --raw-output "* ]]; then
+              RAW_OUTPUT="-r"
+            fi
+
+            EXTRA_ARGS=$(echo "$*" | sed -e 's/--raw-output//g' -e 's/-r//g')
+            if [[ -z "$EXTRA_ARGS" ]]; then
+              EXTRA_ARGS="."
+            fi
+
+            echo "$ARRAY" | jq --argjson index $RAND_INDEX '.kaomoji[$index]' | jq $RAW_OUTPUT "$EXTRA_ARGS"
+          '';
+        };
+      in
+      {
+        packages.default = myprogram;
+
+        apps.default = {
+          type = "app";
+          program = "${myprogram}/bin/generate-kaomoji";
+        };
+      });
+}
